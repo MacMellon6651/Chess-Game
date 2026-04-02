3dc36fbb87e1f8081eba92307deb41c258361c8c
Cyrill Dem <kdem88885@gmail.com>
2026-04-03 00:07:36 +0700

    создан скрипт сборки для Windows

---
+++ build.bat
@@ -0,0 +1,2 @@
+g++ main.cpp server.cpp command.cpp config.cpp logging.cpp -o server.exe -I"D:\practice\Cpp\Chess\boost_1_90_0" -L"D:\practice\Cpp\Chess\boost_1_90_0\stage\lib" -lboost_log-mgw13-mt-x64-1_90 -lboost_log_setup-mgw13-mt-x64-1_90 -lboost_thread -lboost_chrono -lboost_atomic -lws2_32 -lmswsock
+g++ client.cpp command.cpp config.cpp logging.cpp -o client.exe -I"D:\practice\Cpp\Chess\boost_1_90_0" -L"D:\practice\Cpp\Chess\boost_1_90_0\stage\lib" -lboost_log-mgw13-mt-x64-1_90 -lboost_log_setup-mgw13-mt-x64-1_90 -lboost_thread -lboost_chrono -lboost_atomic -lws2_32 -lmswsock
