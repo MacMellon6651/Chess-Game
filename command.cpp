5d93b9dc6e86c3579614271ed6951a4ccba555c0
Cyrill Dem <kdem88885@gmail.com>
2026-04-03 00:07:36 +0700

    реализован парсер JSON-протокола

---
+++ command.cpp
@@ -0,0 +1,39 @@
+#include "command.hpp"
+#include <iostream>
+
+GameCommand Protocol::parse(const std::string& raw_d){
+    GameCommand cmd;
+    try{
+
+        auto jn = nlohmann::json::parse(raw_d);
+
+        if (jn.contains("type")){
+            cmd.type = jn.at("type").get<std::string>();
+            cmd.is_valid = true;
+        }
+
+        cmd.from = jn.value("from","");
+        cmd.to = jn.value("to","");
+        cmd.nick = jn.value("nick","");
+        cmd.text = jn.value("text","");
+        cmd.action = jn.value("action", "");
+
+        cmd.player_id = jn.value("player_id", 0);
+
+    }
+    catch (const nlohmann::json::exception& err){
+        cmd.is_valid = false;
+    }
+    return cmd;
+}
+
+std::string Protocol::serialize(const std::string& status, const std::string& msg){
+    nlohmann::json jn;
+    jn["status"] = status;
+    jn["message"] = msg;
+    return jn.dump() + "\n";
+}
+
+std::string Protocol::serialize_json(const nlohmann::json& jn) {
+    return jn.dump() + "\n";
+}
---
+++ command.hpp
@@ -0,0 +1,32 @@
+// Работа с обработкой json файлов (ходы)
+
+#pragma once 
+#include <string>
+#include "json.hpp"
+
+
+struct GameCommand{
+
+    std::string type; // "auth" "queue" "leave" "move" "chat" "draw" "ping"
+    std::string from; // move: "e2"
+    std::string to;   // move: "e4"
+    std::string nick; // auth
+    std::string text; // chat
+    std::string action; // draw: "offer"|"accept"|"decline"
+    int player_id {};
+    bool is_valid {false}; // Флаг парсинга
+
+};
+
+class Protocol{
+
+    public:
+
+        static GameCommand parse(const std::string& raw_d);
+
+        static std::string serialize(const std::string& status, const std::string& msg);
+
+        static std::string serialize_json(const nlohmann::json& jn);
+
+};
+
