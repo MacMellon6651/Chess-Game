6b294d149b823a51f908fa920d18a8a906d465c9
Cyrill Dem <kdem88885@gmail.com>
2026-04-09 22:58:09 +0700

    Интегрирован шахматный движок в класс GameRoom (добавлено поле Position, вспомогательные функции)

--- server.hpp
+++ server.hpp
@@ -1,114 +1,112 @@
-// ядро сервера (tcp)
+#pragma once
 
-#pragma once 
-
-#include <boost\asio.hpp>
+#include <boost/asio.hpp>
 #include <memory>
 #include <deque>
 #include <vector>
 #include "config.hpp"
 #include "command.hpp"
 #include "logging.hpp"
+#include "chess_engine/position.hpp"
 
 using boost::asio::ip::tcp;
 
 class ChessServer;
 class GameRoom;
 
-class Session: public std::enable_shared_from_this<Session>{
+// Вспомогательные функции для преобразования координат
+uint8_t algebraic_to_index(const std::string& sq);
+std::string index_to_algebraic(uint8_t idx);
 
-    public:
-        explicit Session(tcp::socket socket, ChessServer& server);
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    explicit Session(tcp::socket socket, ChessServer& server);
+    tcp::socket& socket();
+    void start();
+    void send(const nlohmann::json& msg);
+    void close();
 
+    bool is_authorized() const;
+    const std::string& nick() const;
+    int rating() const;
 
-        tcp::socket& socket();
+private:
+    void do_read();
+    void do_write();
+    void handle_command(const std::string& raw, const GameCommand& cmd);
 
-        void start();
+    tcp::socket _socket;
+    boost::asio::streambuf _buffer;
+    boost::asio::strand<boost::asio::any_io_executor> _strand;
+    ChessServer& _server;
 
-        void send(const nlohmann::json& msg);
-        void close();
+    std::deque<std::string> _outbox;
 
-        bool is_authorized() const;
-        const std::string& nick() const;
-        int rating() const;
+    bool _authorized{false};
+    std::string _nick;
+    int _rating{1000};
+    std::weak_ptr<GameRoom> _room;
 
-    private:
-
-        void do_read();
-
-        void do_write();
-
-        void handle_command(const std::string& raw, const GameCommand& cmd);
-
-        tcp::socket _socket;
-        boost::asio::streambuf _buffer;
-        boost::asio::strand<boost::asio::any_io_executor> _strand;
-        ChessServer& _server;
-
-        std::deque<std::string> _outbox;
-
-        bool _authorized {false};
-        std::string _nick;
-        int _rating {1000};
-        std::weak_ptr<GameRoom> _room;
-
-        friend class ChessServer;
-        friend class GameRoom;
-
+    friend class ChessServer;
+    friend class GameRoom;
 };
 
+// Класс игровой комнаты с использованием шахматного движка
 class GameRoom : public std::enable_shared_from_this<GameRoom> {
-    public:
-        GameRoom(std::shared_ptr<Session> a, std::shared_ptr<Session> b);
+public:
+    GameRoom(std::shared_ptr<Session> a, std::shared_ptr<Session> b);
 
-        void start();
-        void on_chat(const std::shared_ptr<Session>& from, const std::string& text);
-        void on_move(const std::shared_ptr<Session>& from, const std::string& from_sq, const std::string& to_sq);
-        void on_draw_request(const std::shared_ptr<Session>& from);
-        bool on_draw_accept(const std::shared_ptr<Session>& from);
-        bool on_draw_decline(const std::shared_ptr<Session>& from);
-        void on_disconnect(const std::shared_ptr<Session>& who);
+    void start();
+    void on_chat(const std::shared_ptr<Session>& from, const std::string& text);
+    bool on_move(const std::shared_ptr<Session>& from, const std::string& from_sq, 
+                 const std::string& to_sq, const std::string& promotion = "");
+    void on_draw_request(const std::shared_ptr<Session>& from);
+    bool on_draw_accept(const std::shared_ptr<Session>& from);
+    bool on_draw_decline(const std::shared_ptr<Session>& from);
+    void on_disconnect(const std::shared_ptr<Session>& who);
 
-        bool has(const std::shared_ptr<Session>& s) const;
-        std::shared_ptr<Session> opponent_of(const std::shared_ptr<Session>& s) const;
-        std::shared_ptr<Session> a() const { return _a; }
-        std::shared_ptr<Session> b() const { return _b; }
+    bool has(const std::shared_ptr<Session>& s) const;
+    std::shared_ptr<Session> opponent_of(const std::shared_ptr<Session>& s) const;
+    
+    // Получение текущей позиции в FEN для отладки
+    std::string get_position_fen() const;
 
-    private:
-        std::shared_ptr<Session> _a;
-        std::shared_ptr<Session> _b;
-        bool _draw_offer_a {false};
-        bool _draw_offer_b {false};
+private:
+    void broadcast(const nlohmann::json& msg);
+    void end_game(const std::string& result, const std::shared_ptr<Session>& winner = nullptr);
+    void update_rating(const std::shared_ptr<Session>& winner, const std::shared_ptr<Session>& loser);
+
+    std::shared_ptr<Session> _a;
+    std::shared_ptr<Session> _b;
+    bool _draw_offer_a{false};
+    bool _draw_offer_b{false};
+    
+    // Шахматный движок
+    Position _position;
 };
 
-class ChessServer{
+class ChessServer {
+public:
+    ChessServer(boost::asio::io_context& io, const ServerSettings& config);
 
+private:
+    void start_accept();
+    void on_disconnect(const std::shared_ptr<Session>& s);
 
-    public:
-        ChessServer(boost::asio::io_context& io, const ServerSettings& config);
+    void handle_auth(const std::shared_ptr<Session>& s, const GameCommand& cmd);
+    void handle_queue(const std::shared_ptr<Session>& s);
+    void handle_leave(const std::shared_ptr<Session>& s);
+    void handle_chat(const std::shared_ptr<Session>& s, const GameCommand& cmd);
+    void handle_move(const std::shared_ptr<Session>& s, const GameCommand& cmd);
+    void handle_draw(const std::shared_ptr<Session>& s, const GameCommand& cmd);
 
-    private:
-        void start_accept();
+    void try_matchmake();
+    void remove_from_queue(const std::shared_ptr<Session>& s);
 
-        void handle_accept(std::shared_ptr<Session> new_session, const boost::system::error_code& err); // shared_ptr разобраться с рвботой (шприная память )
+    tcp::acceptor _acceptor;
+    std::vector<std::shared_ptr<Session>> _sessions;
+    std::vector<std::shared_ptr<GameRoom>> _rooms;
+    std::deque<std::shared_ptr<Session>> _queue;
 
-        void on_disconnect(const std::shared_ptr<Session>& s);
-
-        void handle_auth(const std::shared_ptr<Session>& s, const GameCommand& cmd);
-        void handle_queue(const std::shared_ptr<Session>& s);
-        void handle_leave(const std::shared_ptr<Session>& s);
-        void handle_chat(const std::shared_ptr<Session>& s, const GameCommand& cmd);
-        void handle_move(const std::shared_ptr<Session>& s, const GameCommand& cmd);
-        void handle_draw(const std::shared_ptr<Session>& s, const GameCommand& cmd);
-
-        void try_matchmake();
-        void remove_from_queue(const std::shared_ptr<Session>& s);
-
-        tcp::acceptor _acceptor;
-
-        std::vector<std::shared_ptr<Session>> _sessions;
-        std::vector<std::shared_ptr<GameRoom>> _rooms;
-        std::deque<std::shared_ptr<Session>> _queue;
-
-        friend class Session;
+    friend class Session;
 };
